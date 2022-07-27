6c379681b48ce5e498a9171f07d88776d55f4a52
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-27 09:47:48 -0600

    test: tests added for museum_spec.rb to cover it2

---
+++ spec/museum_spec.rb
@@ -0,0 +1,42 @@
+require './lib/patron'
+require './lib/exhibit'
+require './lib/museum'
+
+RSpec.describe Museum do
+    before :each do
+        @dmns = Museum.new("Denver Museum of Nature and Science")
+        @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+        @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+        @imax = Exhibit.new({name: "IMAX",cost: 15})
+        @patron_1 = Patron.new("Bob", 20)
+        @patron_2 = Patron.new("Sally", 20)
+    end
+
+    it 'exists' do 
+        expect(@dmns).to be_a(Museum)
+    end
+
+    it 'can return the museum name' do
+        expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    end
+
+    it 'can return the current empty array of exhibits at the museum' do
+        expect(@dmns.exhibits).to eq([])
+    end
+
+    it 'can add and return an array of exhibits' do
+        @dmns.add_exhibit(gems_and_minerals)
+        @dmns.add_exhibit(dead_sea_scrolls)
+        @dmns.add_exhibit(imax)
+        expect(@dmns.exhibits.count).to eq(3)
+    end
+
+    it 'can return the interest patrons have' do
+        @patron_1.add_interest("Dead Sea Scrolls")
+        @patron_1.add_interest("Gems and Minerals")
+        @patron_2 = Patron.new("Sally", 20)
+        expect(@dmns.reccomend_exhibits(@patron_1).count).to eq(2)
+        expect(@dmns.reccomend_exhibits(@patron_2).count).to eq(1)
+    end
+
+end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,6 +1,6 @@
 require './lib/patron'
 
-RSpec.describe do
+RSpec.describe Patron do
     before :each do
         @patron_1 = Patron.new("Bob", 20)
     end
